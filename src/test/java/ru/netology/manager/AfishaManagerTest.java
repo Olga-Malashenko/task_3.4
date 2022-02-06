fd619b43e744177c2fbb37f1038090954932b734
Olga Malashenko <olga_birjukova@mail.ru>
2022-02-06 22:39:23 +0300

    Class AfishaManagerTest changed

--- src/test/java/ru/netology/manager/AfishaManagerTest.java
+++ src/test/java/ru/netology/manager/AfishaManagerTest.java
@@ -10,17 +10,15 @@ class AfishaManagerTest {
     public void shouldGetForEmpty() {
         AfishaManager movie = new AfishaManager();
 
-        AfishaManager first = new AfishaManager(movie,1); // что-то я потерялась совсем ((
+        AfishaManager first = new AfishaManager(movies,1); // что-то я потерялась совсем ((
         // где какой класс писать, где какой параметр... НЕ понимаю, как мы передаем этот объект с фильмом в афишу
-//        AfishaManager second = new AfishaManager(2, 2, "second", 1, 1);
-//        AfishaManager third = new AfishaManager(3, 3, "third", 1, 1);
+
         AfishaManager.save();
-//        AfishaManager.save(second);
-//        AfishaManager.save(third);
+
 
 //        movie.save();
 
-        Movie[] actual = AfishaManager.getAll();
+        AfishaManager[] actual = AfishaManager.getAll();
         AfishaManager[] expected = new AfishaManager[]{first};
 
         assertArrayEquals(expected, actual);
