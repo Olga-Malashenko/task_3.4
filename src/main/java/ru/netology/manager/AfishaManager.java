e51b2b0e8f97ba394272771bc9a7c472b67c4864
Olga Malashenko <olga_birjukova@mail.ru>
2022-02-16 14:02:56 +0300

    Files AfishaManager.java fixed

--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -1,19 +1,34 @@
 package ru.netology.manager;
 
 public class AfishaManager {
-    private static Movie[] movies;
-    private static int limitForFeed;
-
-    public AfishaManager() {
-    }
+    private Movie[] movies = new Movie[0];
+    private int limitForFeed = 10;
 
     public AfishaManager(Movie[] movies, int limitForFeed) {
         this.movies = movies;
         this.limitForFeed = limitForFeed;
     }
 
-    public static void save(Movie movie) {
+    public AfishaManager() {
+    }
 
+    public Movie[] getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Movie[] movies) {
+        this.movies = movies;
+    }
+
+    public int getLimitForFeed() {
+        return limitForFeed;
+    }
+
+    public void setLimitForFeed(int limitForFeed) {
+        this.limitForFeed = limitForFeed;
+    }
+
+    public void add(Movie movie) {
         int length = movies.length + 1;
         Movie[] tmp = new Movie[length];
         System.arraycopy(movies, 0, tmp, 0, movies.length);
@@ -22,17 +37,14 @@ public class AfishaManager {
         movies = tmp;
     }
 
-//    public Movie[] findAll() {return movies;}
-
-    public static AfishaManager[] getAll() {
+    public Movie[] getAll() {
         int resultLenght;
-        if (limitForFeed == 0) {
-            resultLenght = 10;
+        if (movies.length < limitForFeed) {
+            resultLenght = movies.length;
         } else {
             resultLenght = limitForFeed;
         }
-        Movie[] result = new Movie[resultLenght]; // не понимаю разницы между Movie[] и AfishaManager[]
-         // когда одно используем, когда другое, или это одно и то же ?
+        Movie[] result = new Movie[resultLenght];
         for (int i = 0; i < resultLenght; i++) {
             int index = resultLenght - i - 1;
             result[i] = movies[index];
